702b59dd374d6b869722319c1ab8d71aee4aca1b
Ahmet <scienceworker34@gmail.com>
2021-04-24 18:14:16 +0300

    add apı url and reuests

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from decouple import config
+import requests
 
-# Create your views here.
+def index(request):
+    url = config("BASE_URL")
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    a = r.json()
+    return render(request, "weather/index.html")
